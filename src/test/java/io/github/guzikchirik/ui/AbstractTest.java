c9deb6f5db4e260911452876efc30804174cb1a7
Anton Chyrko <anton_chyrko@epam.com>
2022-06-29 11:10:09 +0200

    add test with dataprovider

--- src/test/java/io/github/guzikchirik/ui/AbstractTest.java
+++ src/test/java/io/github/guzikchirik/ui/AbstractTest.java
@@ -3,21 +3,24 @@ package io.github.guzikchirik.ui;
 import static com.codeborne.selenide.Selenide.open;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+import com.codeborne.selenide.Selenide;
 
 import io.github.guzikchirik.driver.SelenideManager;
 import io.github.guzikchirik.utils.PropertiesManager;
 
 public abstract class AbstractTest {
 
-    @BeforeClass
-    public void setUp() {
+    @BeforeSuite
+    public void setUpSelenide() {
         new SelenideManager().configure();
-        open(PropertiesManager.getProperty("start.url"));
     }
 
-    @AfterClass
+    @AfterMethod
     public void tearDown() {
-
+        Selenide.clearBrowserCookies();
     }
 }
--- src/test/java/io/github/guzikchirik/ui/triangle/FirstTests.java
+++ src/test/java/io/github/guzikchirik/ui/triangle/FirstTests.java
@@ -1,28 +1,58 @@
 package io.github.guzikchirik.ui.triangle;
 
+import static com.codeborne.selenide.Selenide.open;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.github.guzikchirik.page.LandingPage;
 import io.github.guzikchirik.page.TrianglePage;
 import io.github.guzikchirik.ui.AbstractTest;
+import io.github.guzikchirik.utils.PropertiesManager;
 
 public class FirstTests extends AbstractTest {
 
+    private static final String RIGHT_ANGLED_TRIANGLE_MSG = "It's an right angled triangle!";
+    private static final String ISOSCELES_TRIANGLE_MSG = "It's an isosceles triangle!";
+    private static final String EQUILATERAL_TRIANGLE_MSG = "It's an equilateral triangle!";
+    private static final String TRIANGLE_DOES_NOT_EXIST_MSG = "Such a triangle doesn't exist! " +
+            "A side of a triangle cannot be greater than the sum of the lengths of the other two sides!";
     private TrianglePage trianglePage;
 
     @BeforeMethod
-    public void detUp() {
+    public void setUp() {
+        open(PropertiesManager.getProperty("start.url"));
         trianglePage = new LandingPage().openTrianglePage();
     }
 
-    @Test
-    public void rightAngleTriangleTest() {
-        trianglePage.inputSideA("3");
-        trianglePage.inputSideB("4");
-        trianglePage.inputSideC("5");
+    @Test(dataProvider = "triangles")
+    public void rightAngleTriangleTest(final String a, final String b, final String c, final String expectedMsg) {
+        trianglePage.inputSideA(a);
+        trianglePage.inputSideB(b);
+        trianglePage.inputSideC(c);
         trianglePage.clickShowBtn();
-        Assert.assertEquals(trianglePage.getMessage(), "It's an right angled triangle!", "Wrong message!");
+        Assert.assertEquals(trianglePage.getMessage(), expectedMsg, "Wrong message!");
+    }
+
+    @DataProvider
+    public static Object[][] triangles() {
+        return new Object[][]{
+                {"3", "4", "5", RIGHT_ANGLED_TRIANGLE_MSG},
+                {"3", "5", "4", RIGHT_ANGLED_TRIANGLE_MSG},
+                {"4", "3", "5", RIGHT_ANGLED_TRIANGLE_MSG},
+                {"4", "5", "3", RIGHT_ANGLED_TRIANGLE_MSG},
+                {"5", "3", "4", RIGHT_ANGLED_TRIANGLE_MSG},
+                {"5", "4", "3", RIGHT_ANGLED_TRIANGLE_MSG},
+
+                {"2", "2", "1", ISOSCELES_TRIANGLE_MSG},
+                {"2", "1", "2", ISOSCELES_TRIANGLE_MSG},
+                {"1", "2", "2", ISOSCELES_TRIANGLE_MSG},
+
+                {"5", "5", "5", EQUILATERAL_TRIANGLE_MSG},
+
+                {"2", "2", "5", TRIANGLE_DOES_NOT_EXIST_MSG},
+        };
     }
 }
